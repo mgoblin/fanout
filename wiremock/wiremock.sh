c68ed4044428ba5436a5f6760855f5f6309fcb18
mgoblin <mike.golovanov@gmail.com>
2020-08-06 16:59:57 +0300

    ws stub 500 tps

--- wiremock/wiremock.sh
+++ wiremock/wiremock.sh
@@ -1,8 +1,8 @@
 java -jar wiremock-standalone-2.26.3.jar \
  --port 8080 \
- --async-response-enabled true
-# --async-response-threads 50 \
-# --no-request-journal \
-# --container-threads 15 \
-# --jetty-acceptor-threads 5 \
-# --jetty-accept-queue-size 8096
+ --async-response-enabled true \
+ --async-response-threads 50 \
+ --no-request-journal \
+ --container-threads 20 \
+ --jetty-acceptor-threads 5 \
+ --jetty-accept-queue-size 100000
