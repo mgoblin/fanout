774c022989e48ffc8555d162b68319d87822446b
mgoblin <mike.golovanov@gmail.com>
2020-08-13 08:05:39 +0300

    root request mapping for stub controller

--- src/main/java/ru/mg/fanout/rest/controller/stub/AccountStubController.java
+++ src/main/java/ru/mg/fanout/rest/controller/stub/AccountStubController.java
@@ -3,6 +3,7 @@ package ru.mg.fanout.rest.controller.stub;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -11,6 +12,7 @@ import ru.mg.fanout.rest.model.AccountsResponse;
 import ru.mg.fanout.rest.service.stub.StubRestService;
 
 @RestController
+@RequestMapping("/stub")
 public class AccountStubController {
 
     @Autowired
@@ -21,7 +23,7 @@ public class AccountStubController {
      * @param size размер пачки
      * @return ответ с пачкой и ее размером
      */
-    @GetMapping(path="/stub/package/{size}", produces = "application/json")
+    @GetMapping(path="/package/{size}", produces = "application/json")
     public Mono<AccountsResponse> getAccountsStub(@PathVariable("size") int size) {
         return restAccountService.getAccountsStub(size);
     }
@@ -30,7 +32,7 @@ public class AccountStubController {
      * Получение пачки без обращения в вебсервис в виде flux
      * @return пачка
      */
-    @GetMapping(path="/stub/package/flux", produces = "application/json")
+    @GetMapping(path="/package/flux", produces = "application/json")
     public Flux<Account> getAccountsStubFlux() {
         return restAccountService.getAccountsStubFlux(100);
     }
@@ -39,7 +41,7 @@ public class AccountStubController {
      * Получение единичного ответа без обращения в вебсервис и сборки пачки
      * @return ответ с одним аккаунтом
      */
-    @GetMapping(path="/stub/single", produces = "application/json")
+    @GetMapping(path="/single", produces = "application/json")
     public Mono<Account> getAccountStub() {
         return restAccountService.getAccountStub();
     }
