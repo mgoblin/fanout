d38316c4dd276d6505dd8ddd02c80d06f2a740a7
mgoblin <mike.golovanov@gmail.com>
2020-08-14 19:58:33 +0300

    javadoc

--- src/main/java/ru/mg/fanout/rest/controller/AccountRestController.java
+++ src/main/java/ru/mg/fanout/rest/controller/AccountRestController.java
@@ -5,6 +5,11 @@ import reactor.core.publisher.Mono;
 import ru.mg.accountservice.Account;
 import ru.mg.fanout.rest.model.AccountsResponse;
 
+/**
+ * Базовый интерфейс Spring контроллера
+ * @param <T> единичная сущность ответа
+ * @param <TR> аггрегированный ответ на пачку
+ */
 public interface AccountRestController<T, TR> {
     Mono<ResponseEntity<T>> getAccount();
     Mono<ResponseEntity<TR>> getAccounts(int size);
